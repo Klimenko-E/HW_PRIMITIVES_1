5a89a8049d5b62aae32d1c3a0001ed1141898b15
Klimenko.E <klimenko.e@bk.ru>
2023-07-03 14:25:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 17_635;
+        int milePrice = 20;
+
+        int mile = ticketPrice/milePrice;
+
+        System.out.println(mile);
+
+
+        }
+    }
